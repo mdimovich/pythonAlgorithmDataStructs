6ef4f71901ffa20676b1bcef87054194bc0c8800
Mdimovich <mdimovich@gmail.com>
2017-03-17 18:58:47 -0400

    Added queue functionality

--- queue.py
+++ queue.py
@@ -7,13 +7,25 @@ class Queue:
     def isEmpty(self):
         return self.items == []
 
-    def push(self, value):
-        self.items.append(value)
+    def enqueue(self, value):
+        self.items.insert(0, value)
 
-    def pop(self):
-        self.items.pop()
+    def dequeue(self):
+        return self.items.pop()
 
     def size(self):
         return len(self.items)
 
 
+def hotPotato(namelist, num):
+    simqueue = Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+
+    while simqueue.size() > 1:
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+        simqueue.dequeue()
+
+    return simqueue.dequeue()
+print(hotPotato(["Bill", "Josh", "David", "Sam", "Jennifer", "Brad"], 7))
